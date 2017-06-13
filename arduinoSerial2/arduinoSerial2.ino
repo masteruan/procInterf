a701d8ccaf40c5d75ccc7eb807911f9759ff62c0
masteruan <masteruan@gmail.com>
2017-06-13 19:49:44 +0200

    modified ardu serial

--- arduinoSerial2/arduinoSerial2.ino
+++ arduinoSerial2/arduinoSerial2.ino
@@ -1,22 +1,37 @@
-vois setup()
+/*
+*
+*
+*/
+boolean stringComplete;
+String input = "";
+char inChar;
+
+void setup()
 {
+ pinMode(13, OUTPUT);
+ digitalWrite(13, LOW);
  Serial.begin(9600);
  Serial.flush();
 }
+void seriale() {
+  if (input[0] == 'A'){
+    digitalWrite(13, HIGH);
+  }
+  else if (input == "_spegni\n") {
+    digitalWrite(13, LOW);
+  }
+  }
 
-void loop()
-{
+void loop(){
   if (stringComplete) {
     Serial.println(input);
     seriale();
     input = "";
     stringComplete = false;
   }
+}
 
-  void seriale() {
-  if (input[0] == 'A'){
-    digitalWrite(13, HIGH);
-  }
+
 
   void serialEvent() {
     Serial.flush();
