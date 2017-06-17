247cce56876bbc6b314f8ca6d88a87b2c51071cf
masteruan <masteruan@gmail.com>
2017-06-17 21:28:37 +0200

    seriale invric

--- arduinoSerial2/arduinoSerial2.ino
+++ arduinoSerial2/arduinoSerial2.ino
@@ -2,17 +2,29 @@
 *
 *
 */
+
 boolean stringComplete;
 String input = "";
 char inChar;
 
+boolean sign_uno = true;
+
+boolean sign_due = true;
+
+int games[] = {A0,A1,A2,A3,A4,A5};
+
+
 void setup()
 {
  pinMode(13, OUTPUT);
+ for (int i = 0; i < 6; i++){
+      pinMode(games[i], INPUT_PULLUP);
+    }
  digitalWrite(13, LOW);
  Serial.begin(9600);
  Serial.flush();
 }
+
 void seriale() {
   if (input[0] == 'A'){
     digitalWrite(13, HIGH);
@@ -20,18 +32,33 @@ void seriale() {
   else if (input == "_spegni\n") {
     digitalWrite(13, LOW);
   }
+  else if (input == "_lettura\n") {
+    lettura();
+  }
   }
 
 void loop(){
   if (stringComplete) {
-    Serial.println(input);
+    //Serial.println(input);
     seriale();
     input = "";
     stringComplete = false;
   }
 }
 
+void lettura() {
+  sign_uno = digitalRead(A0);
+  sign_due = digitalRead(A1);
+  delay(10);
+  if (!sign_uno){
+    Serial.print("Gioco 1 OK\n");
+  }
+  if (!sign_due){
+    Serial.print("Gioco 2 OK ");
+  }
+  Serial.print("\r");
 
+}
 
   void serialEvent() {
     Serial.flush();
